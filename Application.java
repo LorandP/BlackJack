9248bdc068ad7e2746950ca5471f8d9b3f660460
Lori <palfalvi.lorand@gmail.com>
2017-02-03 11:06:43 +0200

    Changed the naming convention.

--- Application.java
+++ Application.java
@@ -11,25 +11,25 @@ public class Application {
      * @return the value that is nearest to 21.
      */
     private String blackJack(int firstNumber, int secondNumber) {
-        int reminder_from_first_number = 0;
-        int reminder_from_second_number = 0;
-        String number_as_string;
+        int reminderFromFirstNumber = 0;
+        int reminderFromSecondNumber = 0;
+        String numberAsString;
 
         if (firstNumber < 0 || secondNumber < 0)
             return "Please enter 0 or a positive number.";
-        reminder_from_first_number = 21 - firstNumber;
-        reminder_from_second_number = 21 - secondNumber;
-        if ((reminder_from_first_number < reminder_from_second_number &&
-                reminder_from_first_number >= 0) ||
-                reminder_from_second_number < 0)
-            return number_as_string = Integer.toString(firstNumber);
+        reminderFromFirstNumber = 21 - firstNumber;
+        reminderFromSecondNumber = 21 - secondNumber;
+        if ((reminderFromFirstNumber < reminderFromSecondNumber &&
+                reminderFromFirstNumber >= 0) ||
+                reminderFromSecondNumber < 0)
+            return numberAsString = Integer.toString(firstNumber);
         else
-            return number_as_string = Integer.toString(secondNumber);
+            return numberAsString = Integer.toString(secondNumber);
     }
 
     public static void main(String[] args) {
         Application application = new Application();
 
-        System.out.println(application.blackJack(22, 22));
+        System.out.println(application.blackJack(19, 22));
     }
 }
